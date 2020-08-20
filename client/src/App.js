88d980b2d4fea21a2755e5cb6ad62d0175951300
James Meyer <Jamesmeyer75@gmail>
2020-08-20 11:32:07 -0500

    Removed unnecessary images and code, and formatted all files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 import './App.css';
 
@@ -10,21 +10,21 @@ class App extends Component {
     post: '',
     responseToPost: '',
   };
-  
+
   componentDidMount() {
     this.callApi()
       .then(res => this.setState({ response: res.express }))
       .catch(err => console.log(err));
   }
-  
+
   callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    
+
     return body;
   };
-  
+
   handleSubmit = async e => {
     e.preventDefault();
     const response = await fetch('/api/world', {
@@ -35,18 +35,14 @@ class App extends Component {
       body: JSON.stringify({ post: this.state.post }),
     });
     const body = await response.text();
-    
+
     this.setState({ responseToPost: body });
   };
-  
-render() {
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -56,19 +52,21 @@ render() {
             Learn React
           </a>
         </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        <div className="center-up">
+          <p className="pad-bottom">{this.state.response}</p>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <strong>Post to Server:</strong>
+            </p>
+            <input
+              type="text"
+              value={this.state.post}
+              onChange={e => this.setState({ post: e.target.value })}
+            />
+            <button className="pad-right" type="submit">Submit</button>
+          </form>
+          <p>{this.state.responseToPost}</p>
+        </div>
       </div>
     );
   }
